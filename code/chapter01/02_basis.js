73b10b754e111e756fcf85c9b5afcf2458d49fa6
lj <2385583808@qq.com>
2022-08-16 18:16:28 +0800

    saefsefeg

---
+++ code/chapter01/02_basis.js
@@ -0,0 +1,23 @@
+// 声明一个变量a，同时指定它的类型为number
+// let a :Number;
+// a 的类型为number，在以后的使用过程中a的值只能是数字
+// a = 10;
+// a = 33;
+// a = true; // 会报错
+// let b: string;
+// b = 'hello';
+// 声明完变量直接进行赋值
+// let c:Boolean = true;
+// 如果变量的声明和赋值是同时进行的。Ts可以自动对变量进行类型检测
+// let c = false;
+// c = true;
+// js中函数是不考虑参数类型和个数的
+// function sum(a, b){
+//     return a+ b;
+// }
+// console.log(sum(123,456));
+// console.log(sum(123,"456"));
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(123, 222));
---
+++ code/chapter01/02_basis.ts
@@ -0,0 +1,32 @@
+// 声明一个变量a，同时指定它的类型为number
+// let a :Number;
+// a 的类型为number，在以后的使用过程中a的值只能是数字
+// a = 10;
+// a = 33;
+// a = true; // 会报错
+
+// let b: string;
+// b = 'hello';
+
+// 声明完变量直接进行赋值
+// let c:Boolean = true;
+
+// 如果变量的声明和赋值是同时进行的。Ts可以自动对变量进行类型检测
+// let c = false;
+// c = true;
+
+// js中函数是不考虑参数类型和个数的
+// function sum(a, b){
+//     return a+ b;
+// }
+// console.log(sum(123,456));
+// console.log(sum(123,"456"));
+
+// 可以给参数做类别的声明
+function sum(a:number,b:number):number{
+    return a+b;
+}
+console.log(sum(123,222));
+
+
+let result = sum(123,222);
