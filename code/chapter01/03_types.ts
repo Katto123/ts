55e852d5bb6e70f727129b0d094fe124ea4b7997
lj <2385583808@qq.com>
2022-08-17 17:44:01 +0800

    saefsefeg

--- code/chapter01/03_types.ts
+++ code/chapter01/03_types.ts
@@ -1,103 +1,103 @@
-// 也可以直接使用字面量进行类型声明
+// // 也可以直接使用字面量进行类型声明
 
-// let a: string;
-// let a : 10 //设置a的类型只能为10
-// a = 11
-
-
-
-let b: "male" | "female"  // b的值只能是male或者female  可以使用 | 来连接多个类型(联合类型)
-b = "male"
-b = "female"
+// // let a: string;
+// // let a : 10 //设置a的类型只能为10
+// // a = 11
 
 
 
-let c: boolean | string  // 俩个类型
-c = true
-c = "wqeqw"
+// let b: "male" | "female"  // b的值只能是male或者female  可以使用 | 来连接多个类型(联合类型)
+// b = "male"
+// b = "female"
 
 
 
-// any 表示的是任意类型， 一个变量设置类型为any相当于对该变量关闭了TS的类型检测
-// let d:any;
-
-
-// 声明变量如果不指定类型，则TS解析器会自动判断变量类型为any（隐式的any）
-let d;
-d = 10;
-d = 'hello'
-d = true
+// let c: boolean | string  // 俩个类型
+// c = true
+// c = "wqeqw"
 
 
 
-// unknown  表示未知类型的值
-let e: unknown
-e = 10;
-e = "fnwfnwe"
-
-let s: string
-// d的类型是any， 它可以赋值给任意变量
-s = d
+// // any 表示的是任意类型， 一个变量设置类型为any相当于对该变量关闭了TS的类型检测
+// // let d:any;
 
 
+// // 声明变量如果不指定类型，则TS解析器会自动判断变量类型为any（隐式的any）
+// let d;
+// d = 10;
+// d = 'hello'
+// d = true
 
 
 
-//   unknown  实际上就是一个类型安全的any
-//    unknown  类型的变量，不能直接赋值给其他变量
+// // unknown  表示未知类型的值
+// let e: unknown
+// e = 10;
+// e = "fnwfnwe"
 
-// 如果想赋值可以在外面加一个判断
-
-if (typeof e === "string") {
-    s = e
-}
+// let s: string
+// // d的类型是any， 它可以赋值给任意变量
+// s = d
 
 
 
 
 
-// 类型断言，可以用来告诉解析器变量的实际类型
-/*
-* 语法：
-* 变量 as 类型
-*   <类型>变量
-**/
+// //   unknown  实际上就是一个类型安全的any
+// //    unknown  类型的变量，不能直接赋值给其他变量
 
-s = e as string  //声明 e 一定是string类型
-s = <string>e; //效果一样
+// // 如果想赋值可以在外面加一个判断
 
-
-
-// 函数自动判断返回值类型
-
-// function fn(num){
-//     if(num > 0){
-//         return true;
-//     }else{
-//         return 123;
-//     }
+// if (typeof e === "string") {
+//     s = e
 // }
 
 
 
 
-// void 用来表示空，以函数为例，就表示没有返回值的函数  空值
-function fn() :void{
-// return 1213
-return
-return undefined
-return null
-}
+
+// // 类型断言，可以用来告诉解析器变量的实际类型
+// /*
+// * 语法：
+// * 变量 as 类型
+// *   <类型>变量
+// **/
+
+// s = e as string  //声明 e 一定是string类型
+// s = <string>e; //效果一样
+
+
+
+// // 函数自动判断返回值类型
+
+// // function fn(num){
+// //     if(num > 0){
+// //         return true;
+// //     }else{
+// //         return 123;
+// //     }
+// // }
+
+
+
+
+// // void 用来表示空，以函数为例，就表示没有返回值的函数  空值
+// function fn() :void{
+// // return 1213
+// return
+// return undefined
+// return null
+// }
 
 
 
 
 
-// never 表示永远不会返回结果
+// // never 表示永远不会返回结果
 
-function fn2(): never{
-     throw new Error('报错了')   // 适合写报错函数
-}
+// function fn2(): never{
+//      throw new Error('报错了')   // 适合写报错函数
+// }
 
 
 
---
+++ code/chapter01/04_types.js
@@ -0,0 +1,45 @@
+// object 表示一个js对象
+var a;
+a = {};
+a = function () { };
+//  用来指定对象中包含哪些属性
+// 语法：{属性名：属性值，属性名：属性值}
+// 在属性名后面加上？，表示属性是可选的
+var b;
+b = { name: 'dek v' };
+b = { name: 'cfefe', age: 12 };
+// [propName:string]: any 表示任意类型的属性
+var c;
+c = { name: "猪八戒", age: 12, gender: '男' };
+// 设置函数结构的类型声明：
+// 语法：（形参：类型，形参：类型...）=>返回值
+var d;
+d = function (a, b) {
+    return 10;
+};
+// 数组的类型声明
+// 1.  类型[]
+// 2.   Array<类型>
+// string[]  表示字符串数组
+var e;
+e = ['a', 'b', 'c'];
+//number[] 表示数值数组
+var f;
+// 都是表示数值类型的数组
+var g;
+// 元组 ，元组就是固定长度的数组
+// 语法: [类型， 类型， 类型]
+var h;
+h = ['hello', 123];
+// enum枚举
+var Gender;
+(function (Gender) {
+    Gender[Gender["Male"] = 0] = "Male";
+    Gender[Gender["Female"] = 1] = "Female";
+})(Gender || (Gender = {}));
+var i;
+i = {
+    name: "孙悟空",
+    gender: Gender.Female // 'male'
+};
+console.log(i.gender === Gender.Male);
